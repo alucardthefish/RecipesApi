1d3c4323271e8cc59826ae7bf54a88cda9faaf70
alucardthefish <sergiop18@gmail.com>
2021-10-19 21:25:18 -0500

    Play with ngFor

--- ui/src/app/components/home/home.component.ts
+++ ui/src/app/components/home/home.component.ts
@@ -9,9 +9,64 @@ export class HomeComponent implements OnInit {
 
   public title = "Home";
 
+  public people = [
+    {
+      name: "Amanda",
+      apellidos: "Paz Espana",
+      signo: "Libra"
+    },
+    {
+      name: "Nubia",
+      apellidos: "Paz Espana",
+      signo: "Leo"
+    },
+    {
+      name: "Fanny",
+      apellidos: "Ortiz Paz",
+      signo: "Sagitario"
+    },
+    {
+      name: "Gladys",
+      apellidos: "Paz Espana",
+      signo: "Virgo"
+    },
+    {
+      name: "Sergio",
+      apellidos: "Ortiz Paz",
+      signo: "Tauro"
+    },
+    {
+      name: "Alejandro",
+      apellidos: "Paz Villamizar",
+      signo: "Virgo"
+    },
+    {
+      name: "Camilo",
+      apellidos: "Mena Paz",
+      signo: "Acuario"
+    },
+    {
+      name: "Salomon",
+      apellidos: "Paz Villamizar",
+      signo: "Tauro"
+    },
+    {
+      name: "Abelardo",
+      apellidos: "Ortiz Ceballos",
+      signo: "Capricornio"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log("Home Component");
+  }
+
+  get filterByZodiac() {
+    return this.people.filter(
+      x => x.signo == "Tauro"
+    );
   }
 
 }
